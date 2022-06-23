6e0c78160fd1fabc1082a821cd540d85da65997a
Brian McCombs <tandis12@hotmail.com>
2022-06-23 13:51:18 -0400

    Add project files.

---
+++ TestEureka/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+using TestEureka;
+
+IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+
+hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    IHostEnvironment hostingEnvironment = hostingContext.HostingEnvironment;
+
+    ConfigServerClientSettings clientSettings = new ConfigServerClientSettings()
+    {
+        Name = "TestEureka",
+        Environment = hostingEnvironment.EnvironmentName,
+        FailFast = true,
+        RetryEnabled = true,
+        RetryInitialInterval = 4000,
+        RetryMultiplier = 2,
+        RetryMaxInterval = 60000,
+        RetryAttempts = 6
+    };
+
+    config
+        .AddJsonFile($"appsettings.json", optional: true)
+        .AddConfigServer(clientSettings)
+        .AddEnvironmentVariables()
+        .Build();
+});
+
+hostBuilder.ConfigureServices((hostContext, services) =>
+{
+    services.AddOptions();
+    services.AddDiscoveryClient();
+    services.AddHostedService<StupidBackgroundService>();
+});
+
+using IHost host = hostBuilder.Build();
+await host.RunAsync();
---
+++ TestEureka/StupidBackgroundService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Discovery;
+
+namespace TestEureka
+{
+    internal class StupidBackgroundService : IHostedService
+    {
+        private bool _running = true;
+        private readonly IServiceProvider _serviceProvider;
+
+        public StupidBackgroundService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _ = BackgroundTask();
+            return Task.CompletedTask;
+        }
+
+        private async Task BackgroundTask()
+        {
+            IDiscoveryClient discoveryClient = _serviceProvider.GetRequiredService<IDiscoveryClient>();
+
+            while (_running)
+            {
+                Console.WriteLine("Hello");
+                Console.WriteLine($"{discoveryClient.Description}");
+                await Task.Delay(2000);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _running = false;
+            return Task.CompletedTask;
+        }
+    }
+}
